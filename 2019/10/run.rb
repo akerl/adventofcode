692c5aba32b233d77dd8e248fda62386a4314abd
Les Aker <me@lesaker.org>
2019-12-11 10:22:20 +0000

    more day 10 attempts

--- 2019/10/run.rb
+++ 2019/10/run.rb
@@ -1,5 +1,16 @@
 require_relative '../lib/helpers.rb'
 
+QUADRANT_ORDER = {
+  [0, 1] => 1,
+  [1, 1] => 2,
+  [1, 0] => 3,
+  [1, -1] => 4,
+  [0, -1] => 5,
+  [-1, -1] => 6,
+  [-1, 0] => 7,
+  [-1, 1] => 8
+}
+
 def get_dir(a)
   if a > 0
     1
@@ -10,30 +21,20 @@ def get_dir(a)
   end
 end
 
-def get_slope_old(a, b)
-  return nil if a == b
-  dx = a[1] - b[1]
-  dy = a[0] - b[0]
-  slope = dx == 0 ? nil : (dy.to_f/dx.to_f.round(2))
-  [slope, get_dir(dx), get_dir(dy)]
+def get_quadrant(x, y)
+  QUADRANT_ORDER[[get_dir(x), get_dir(y)]]
 end
 
 def get_slope(a, b)
   return nil if a == b
-  dx = b[1] - a[1]
-  dy = b[0] - a[0]
-  return dy < 0 ? 0.0 : 180.0 if dx == 0
-  at = Math.atan(dy/dx) * 180 / Math::PI
-  if dx > 0
-    at + 90
-  else
-    at + 270
-  end
+  dy = b[1] - a[1]
+  dx = b[0] - a[0]
+  slope = dx == 0 ? nil : (dy.to_f/dx.to_f.round(2))
+  [slope, get_quadrant(dx, dy), dx.abs+dy.abs]
 end
 
-
 def get_visible_from_point(array, point)
-  array.map { |x| [x, get_slope_old(point, x)] }.uniq { |x| x.last }
+  array.map { |x| [x, get_slope(point, x)] }.group_by { |_, x| x ? [x[0], x[1]] : nil }
 end
 
 def get_most_visible(array)
@@ -44,7 +45,7 @@ asteroids = []
 input_lines.each_with_index do |line, row_index|
   line.each_char.each_with_index do |char, col_index|
     next if char == '.'
-    asteroids << [row_index, col_index]
+    asteroids << [col_index, -1 * row_index]
   end
 end
 
